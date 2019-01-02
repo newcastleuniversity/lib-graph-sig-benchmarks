6f839f7fbbc6506b3a0c8345b5c6a870db4ad1a0
Ioannis Sfyrakis <ioannis.sfyrakis@newcastle.ac.uk>
2019-01-02 18:11:56 +0000

    initial analysis of performance of commitments computations

---
+++ analysis/commitments.R
@@ -0,0 +1,184 @@
+# key generation results analysis
+
+# packages
+# install.packages('DMwR')
+# install.packages("gplots")
+# install.packages('DAAG')
+#install.packages("agricolae")
+library(agricolae)
+library(ggplot2)
+library(gplots)
+library(DAAG)
+library(e1071)
+library(dplyr)
+library(outliers)
+library(plyr)
+library(reshape2)
+library(car)
+
+options(digits=10)
+
+# read data
+dataFile <- "results-commitment-raw-2018-12-27_03-17-46"
+dataPath <- paste0("../data/", dataFile, ".csv", collapse = "" )
+commitmentData <- read.csv(dataPath, sep=",")
+
+# rename headings 
+col_headings <- c('Benchmark','Mode', 'Score', 'Unit', 'Vertices', 'KeyLength')
+names(commitmentData) <- col_headings
+str(commitmentData)
+
+scatter.smooth(x=commitmentData$KeyLength, y=commitmentData$Score, main="Keylength ~ Score")  # scatterplot
+
+#fKeyLength <- factor(commitmentData$KeyLength)
+#fKeyLength
+
+
+summary(commitmentData$Score)
+max(commitmentData$Score)
+table(commitmentData$Score)
+hist(commitmentData$Score)
+
+kmean <- mean(commitmentData$Score)
+kmean
+
+ggplot(commitmentData, aes(x = commitmentData$Score)) + 
+  geom_histogram(binwidth = 2)
+
+ggplot(commitmentData, aes(x = commitmentData$Score)) + 
+  geom_density(adjust = 0.25)
+
+ggplot(commitmentData, aes(x=factor(commitmentData$KeyLength), y=commitmentData$Score)) +
+  stat_boxplot(geom ='errorbar', color="black") +
+  geom_boxplot(fill="cornflowerblue", color="black", notch=FALSE) +
+  geom_point(position="jitter", color="blue", alpha=.5) +
+  geom_rug( color="black") + 
+  labs( x="Key size",  y = "computing commitments time (msec)", fill="") 
+
+ggplot(commitmentData, aes(x=factor(commitmentData$KeyLength), y=commitmentData$Score, fill=commitmentData$KeyLength)) +
+  stat_boxplot(geom ='errorbar', color="black") +
+  geom_boxplot(outlier.colour="black", outlier.shape=18, outlier.size=4, notch=FALSE, fatten=2) + 
+  labs( x="Key size",  y = "Computing commitments time (sec)", fill="") 
+
+# calculate means
+(means<- round(tapply(commitmentData$Score, commitmentData$KeyLength, mean), digits=2)) 
+
+# plot means
+plotmeans(commitmentData$Score ~ commitmentData$KeyLength, digits=2, ccol="red", mean.labels=T, main="Plot of computing commitments means by key length")
+
+boxplot(commitmentData$Score ~ commitmentData$KeyLength, main="Key generation by key length (mean is black dot)", xlab="key length", ylab="execution time (sec) for generating keys", col=rainbow(7))
+
+
+# create density plots
+plot(density(commitmentData$Score), main="Density Plot: Score", ylab="Frequency", sub=paste("Skewness:", round(e1071::skewness(commitmentData$Score), 2)))  # density plot for 'speed'
+polygon(density(commitmentData$Score), col="red")
+
+(aov_cont<- aov(commitmentData$Score ~ factor(commitmentData$KeyLength)))
+ls(aov_cont)
+summary(aov_cont)
+confint(aov_cont, level = 0.9)
+summary.lm(aov_cont)
+plot(aov_cont)
+outlierTest(aov_cont)
+
+# perform Tukey test
+(tuk<- TukeyHSD(aov_cont, conf.level = 0.95))
+plot(tuk)
+
+lmo <- lm(commitmentData$Score ~ commitmentData$KeyLength)
+summary(lmo)
+coef(lmo)
+confint(lmo, level = 0.9)
+anova(lmo)
+plot(lmo)
+outlierTest(lmo)
+
+
+# discard outliers 
+commitmentData$Score<-ifelse(commitmentData$Score==outlier(commitmentData$Score),NA,commitmentData$Score)
+commitmentData
+
+influence.measures(lmo)
+
+ares <- aov(commitmentData$Score ~ commitmentData$KeyLength, data = commitmentData)
+TukeyHSD(ares)
+plot(TukeyHSD(ares), las=0)
+summary(ares)
+
+kruskal.test(commitmentData$Score ~ commitmentData$KeyLength, commitmentData)
+
+barplot(commitmentData$Score)
+(counts <- table(commitmentData$KeyLength))
+
+barplot(counts,main=" Bar Plot",         xlab="Key Length", ylab="Frequency",          col=c("red", "yellow","green", "blue"),         legend=rownames(counts))
+
+meansg <- aggregate(commitmentData$Score, by=list(commitmentData$KeyLength), FUN=mean)
+meansg
+barplot(meansg$x, names.arg=meansg$Group.1,main="Means bar plot", xlab="Key Length", ylab="Mean",          col=c("red", "yellow","green", "blue"))
+
+plot(commitmentData$KeyLength)
+
+res <- HSD.test(ares, 'keyGenTime$KeyLength')
+res
+plot(res)
+
+posthoc <- TukeyHSD(x=aov_cont, 'keyGenTime$KeyLength', conf.level=0.95)
+plot(posthoc)
+
+leveneTest(commitmentData$Score ~ commitmentData$KeyLength, commitmentData)
+
+data_summary <- function(data, varname, groupnames){
+   require(plyr)
+   summary_func <- function(x, col){
+     c(mean = mean(x[[col]], na.rm=TRUE),
+       sd = sd(x[[col]], na.rm=TRUE), 
+       N = length(x[[col]]),
+       se = sd(x[[col]], na.rm=TRUE)/sqrt(length(x[[col]]))
+       ) 
+      
+   }
+   data_sum <- ddply(data, groupnames, .fun=summary_func, varname)
+   #data_sum <- rename(data_sum, c("mean" = varname))
+   return(data_sum)
+ }
+   
+ (df2 <- data_summary(commitmentData, varname = "Score", groupnames = c("KeyLength")))
+ 
+ 
+ (mean(commitmentData$Score, na.rm=TRUE))
+ (sd(commitmentData$Score, na.rm=TRUE))
+
+ggplot(df2, aes(x=df2$KeyLength, y=df2$mean,  fill = df2$KeyLength)) +
+  geom_bar(stat="identity", color="black", position=position_dodge())+
+  theme_minimal() +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.15,
+                position=position_dodge(0.9)) +
+  labs( x="Key length",  y = "Mean key generation time (sec)", fill="") 
+
+
+# calculate quantiles
+quantile(commitmentData$Score)
+t.test(commitmentData$Score, mu=95)
+pairwise.t.test(commitmentData$Score, keyGenTime$KeyLength)
+
+glimpse(commitmentData)
+
+(totalMean <- mean(commitmentData$Score, na.rm=TRUE))
+
+
+# demo <- keyGenTime %>%
+#   #mutate(m = mean(Score, na.rm=TRUE)) %>%
+#   #m = mean(keyGenTime$Score) %>%
+#   group_by(KeyLength) 
+#   summarise(mean = mean(Score, na.rm=TRUE), 
+#             standardDeviation = sd(Score, na.rm = TRUE), 
+#             standardError =  (sd(Score, na.rm=TRUE)/sqrt(n())))
+# demo
+# dd <- demo %>%
+#   mutate(effect = totalMean - mean)
+# dd
+# 
+# 
+# glimpse(dd)
+# add key lenght sizes 
+
