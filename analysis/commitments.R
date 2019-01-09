2e6f9e82aa14f6525cce3e61537b3264f3ed1792
Ioannis Sfyrakis <ioannis.sfyrakis@newcastle.ac.uk>
2019-01-09 15:21:49 +0000

    add bar charts for analysis of commiment computations

--- analysis/commitments.R
+++ analysis/commitments.R
@@ -19,7 +19,7 @@ library(car)
 options(digits=10)
 
 # read data
-dataFile <- "results-commitment-raw-2018-12-27_03-17-46"
+dataFile <- "results-commitment-raw-2019-01-02_07-02-11"
 dataPath <- paste0("../data/", dataFile, ".csv", collapse = "" )
 commitmentData <- read.csv(dataPath, sep=",")
 
@@ -35,8 +35,7 @@ max(commitmentData$Score)
 table(commitmentData$Score)
 hist(commitmentData$Score)
 
-kmean <- mean(commitmentData$Score)
-kmean
+(kmean <- mean(commitmentData$Score))
 
 ggplot(commitmentData, aes(x = commitmentData$Score)) + 
   geom_histogram(binwidth = 2)
@@ -44,23 +43,49 @@ ggplot(commitmentData, aes(x = commitmentData$Score)) +
 ggplot(commitmentData, aes(x = commitmentData$Score)) + 
   geom_density(adjust = 0.25)
 
-ggplot(commitmentData, aes(x=factor(commitmentData$KeyLength), y=commitmentData$Score)) +
+ggplot(commitmentData, aes(x=factor(commitmentData$KeyLength), y=commitmentData$Score/1000)) +
   stat_boxplot(geom ='errorbar', color="black") +
   geom_boxplot(fill="cornflowerblue", color="black", notch=FALSE) +
   geom_point(position="jitter", color="blue", alpha=.5) +
   geom_rug( color="black") + 
-  labs( x="Key size",  y = "computing commitments time (msec)", fill="") 
+  labs( x="Key size",  y = "computing commitments time (msec)", fill="Graph size") 
 
-ggplot(commitmentData, aes(x=factor(commitmentData$KeyLength), y=commitmentData$Score, fill=commitmentData$KeyLength)) +
+ggplot(commitmentData, aes(x=factor(commitmentData$KeyLength), y=commitmentData$Score/1000, fill=factor(commitmentData$Vertices))) +
   stat_boxplot(geom ='errorbar', color="black") +
   geom_boxplot(outlier.colour="black", outlier.shape=18, outlier.size=4, notch=FALSE, fatten=2) + 
-  labs( x="Key size",  y = "Computing commitments time (sec)", fill="") 
+  labs( x="Key size",  y = "Computing commitments time (msec)", fill="Graph size") 
 
 # calculate means
 (means<- round(tapply(commitmentData$Score, commitmentData$KeyLength, mean), digits=2)) 
 
 # plot means
-plotmeans(commitmentData$Score ~ commitmentData$KeyLength, digits=2, ccol="red", mean.labels=T, main="Plot of computing commitments means by key length")
+plotmeans(commitmentData$Score/1000 ~ commitmentData$KeyLength, digits=2, ccol="red", xlab = "Key size", ylab="computing time for commitments (msec)",  mean.labels=T, main="Plot of computing commitments means by key length")
+
+(cdata <- ddply(commitmentData, c("KeyLength", "Vertices", "Score"), summarise,
+               N    = sum(!is.na(commitmentData$Score)),
+               mean = mean(commitmentData$Score, na.rm=TRUE),
+               sd   = sd(commitmentData$Score, na.rm=TRUE),
+               se   = sd / sqrt(N)
+))
+
+
+ggplot(commitmentData, aes(x = factor(commitmentData$KeyLength), y = commitmentData$Score, fill = factor(commitmentData$Vertices))) + 
+        geom_bar(stat="identity", position = "dodge") + 
+        labs( x="Key size",  y = "Computing commitments time (μsec)", fill="Graph size") + 
+        scale_fill_brewer(palette="Pastel1")
+
+#stacked bar chart
+ggplot(commitmentData, aes(x = commitmentData$KeyLength, y = commitmentData$Score, fill = factor(commitmentData$Vertices))) + 
+  geom_bar(stat="identity") + 
+  labs( x="Key size",  y = "Computing commitments time (μsec)", fill="Graph size") + 
+  scale_fill_brewer(palette="Pastel1")
+
+#proportional stacked bar chart
+library(dplyr)
+ce <- ddply(commitmentData, "KeyLength", transform, percent_time = Score / sum(Score) * 100)
+
+ggplot(ce, aes(x=KeyLength, y = percent_time, fill= factor(Vertices))) +
+  geom_bar(stat="identity")
 
 boxplot(commitmentData$Score ~ commitmentData$KeyLength, main="Key generation by key length (mean is black dot)", xlab="key length", ylab="execution time (sec) for generating keys", col=rainbow(7))
 
