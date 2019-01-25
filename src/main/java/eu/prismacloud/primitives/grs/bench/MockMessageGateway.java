dcdaace1e30901371ac60511a3c87241d3a774d2
Ioannis Sfyrakis <ioannis.sfyrakis@newcastle.ac.uk>
2019-01-25 01:47:31 +0000

    update mock gateway

--- src/main/java/eu/prismacloud/primitives/grs/bench/MockMessageGateway.java
+++ src/main/java/eu/prismacloud/primitives/grs/bench/MockMessageGateway.java
@@ -2,21 +2,18 @@ package eu.prismacloud.primitives.grs.bench;
 
 import eu.prismacloud.primitives.zkpgs.message.GSMessage;
 import eu.prismacloud.primitives.zkpgs.message.IMessageGateway;
-import eu.prismacloud.primitives.zkpgs.util.GSLoggerConfiguration;
+import eu.prismacloud.primitives.zkpgs.store.URN;
 
 import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.logging.Logger;
 
 /**
- * Mock gateway proxy for testing orchestrators
+ * Mock gateway proxy for testing orchestrator
  */
 public class MockMessageGateway implements IMessageGateway {
-	private Logger gslog = GSLoggerConfiguration.getGSlog();
-	private GSMessage message;
-	private static int i;
 	private Deque<GSMessage> messageList = new ArrayDeque<>();
+	private GSMessage temp;
 
 	public MockMessageGateway(String type, String hostAddress, Integer portNumber) {
 	}
@@ -24,21 +21,33 @@ public class MockMessageGateway implements IMessageGateway {
 	@Override
 	public void init() throws IOException {
 //		gslog.info("init");
+
 	}
 
 	@Override
 	public void send(GSMessage message) throws IOException {
 //		gslog.info("send message: " + message.getMessageElements() + "\n");
-		messageList.addFirst(message);
+
+		temp = new GSMessage();
+		if (message.getMessageElements().get(URN.createUnsafeZkpgsURN("proof.request")) != null && !messageList.contains(temp)) {
+			messageList.addFirst(message);
+		}
+
+		if (message.getMessageElements().size() >= 1) {
+			messageList.addFirst(message);
+		}
+
 	}
 
 	@Override
 	public GSMessage receive() throws IOException {
 		GSMessage gsMessage = new GSMessage();
+
 		if (messageList != null && !messageList.isEmpty()) {
+//			System.out.println("contains: " + messageList.contains(temp));
 			gsMessage = messageList.pollLast();
-//			gslog.info("receive message: " + gsMessage.getMessageElements() + "\n");
 		}
+
 		return gsMessage;
 	}
 
