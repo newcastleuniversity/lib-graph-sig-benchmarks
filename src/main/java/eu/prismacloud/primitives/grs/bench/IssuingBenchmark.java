e214430e20af19676af646eb52d87c1737ec0586
Ioannis Sfyrakis <ioannis.sfyrakis@newcastle.ac.uk>
2019-01-30 14:27:49 +0000

    update issuing benchmark

--- src/main/java/eu/prismacloud/primitives/grs/bench/IssuingBenchmark.java
+++ src/main/java/eu/prismacloud/primitives/grs/bench/IssuingBenchmark.java
@@ -16,6 +16,7 @@ import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
 import eu.prismacloud.primitives.zkpgs.store.URN;
 import eu.prismacloud.primitives.zkpgs.util.BaseCollection;
 import eu.prismacloud.primitives.zkpgs.util.FilePersistenceUtil;
+import net.nicoulaj.jmh.profilers.SolarisStudioProfiler;
 import org.jgrapht.io.ImportException;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.results.RunResult;
@@ -36,7 +37,6 @@ import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
-import java.util.Vector;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -45,15 +45,13 @@ import java.util.concurrent.TimeUnit;
  */
 
 @State(Scope.Benchmark)
-public class IssuingBenchmark  {
+public class IssuingBenchmark {
 	public static final String DATA_RESULTS_ISSUING_RAW_CSV = "data/results-issuing-raw";
 
-	private static SignerOrchestrator signer;
-	private static RecipientOrchestrator recipient;
 	@Param({"512", "1024", "2048", "3072"})
 	private int l_n;
 
-	@Param({"100", "1000", "10000", "100000"})
+	@Param({"200", "2000", "20000"})
 	private int bases;
 
 	//	@Param({"10", "100", "1000", "10000"})
@@ -62,103 +60,85 @@ public class IssuingBenchmark  {
 	//	@Param({"50", "500", "5000", "50000"})
 	private int l_E;
 
+	private static SignerOrchestrator signer;
+	private static RecipientOrchestrator recipient;
 	private KeyGenParameters keyGenParameters;
 	private SignerKeyPair gsk;
 	private ExtendedPublicKey epk;
-	private BaseCollection baseCollection;
-	private BigInteger randomness;
 	private FilePersistenceUtil persistenceUtil;
 	private GraphEncodingParameters graphEncParams;
 	private ExtendedKeyPair ekp;
-	private Map<URN, BaseRepresentation> encodedBases;
 	private MockMessageGateway messageGateway;
-	private ProverOrchestratorPerf prover;
-	private VerifierOrchestrator verifier;
 	private static String sigmaFilename;
 
-
 	public void setup() throws IOException, EncodingException, ClassNotFoundException, InterruptedException, ExecutionException, ProofStoreException, NoSuchAlgorithmException, VerificationException, ImportException, NoSuchFieldException, IllegalAccessException {
-			// calculate number of bases for vertices and edges
-			l_V = bases / 5;
-			l_E = bases - (bases / 5);
-			 sigmaFilename = "signer-infra-" + l_n + "-" + l_V + "-" + l_E + ".ser";
-			String hostAddress = "192.168.0.19";
-			int portNumber = 9998;
 
-			messageGateway = new MockMessageGateway(DefaultValues.CLIENT, hostAddress, portNumber);
+		// calculate number of bases for vertices and edges
+		l_V = bases / 4;
+		l_E = bases - (bases / 4); //- (bases / 5);
+//		System.out.println("l_V: " + l_V);
+//		System.out.println("l_E: " + l_E);
+		sigmaFilename = "signer-infra-" + l_n + "-" + l_V + "-" + l_E + ".ser";
+		String hostAddress = "192.168.0.19";
+		int portNumber = 9998;
+		messageGateway = new MockMessageGateway(DefaultValues.CLIENT, hostAddress, portNumber);
+		setupIssuing(sigmaFilename);
 
-			setupIssuing(sigmaFilename);
-//			prover = new ProverOrchestratorPerf(ekp.getExtendedPublicKey(), messageGateway);
-//			verifier = new VerifierOrchestrator(ekp.getExtendedPublicKey(), messageGateway);
-//			prover.readSignature(sigmaFilename);
-//			Vector<Integer> vector = new Vector<>();
-//			// add proof vectors
-//			vector.add(1);
-//			vector.add(12);
-//			verifier.createQuery(vector);
-//			verifier.init();
-//			prover.init();
+	}
 
+	public void setupIssuing(String sigmaFilename) throws IOException, ClassNotFoundException, EncodingException, ProofStoreException, NoSuchAlgorithmException, ImportException, VerificationException {
+
+		String signerKeyPairFilename = "SignerKeyPair-" + l_n + ".ser";
+		String signerPKFilename = "SignerPublicKey-" + l_n + ".ser";
+		String ekpFilename = "ExtendedKeyPair-" + l_n + "-" + l_V + "-" + l_E + ".ser";
+		String epkFilename = "ExtendedPublicKey-" + l_n + "-" + l_V + "-" + l_E + ".ser";
+
+		persistenceUtil = new FilePersistenceUtil();
+		gsk = new SignerKeyPair();
+
+		keyGenParameters = KeyGenParameters.createKeyGenParameters(l_n, 1632, 256, 256, 1, 597, 120, 2724, 80, 256, 80, 80);
+
+		graphEncParams = new GraphEncodingParameters(l_V, 56, l_E, 256, 16);
+
+		if (!new File(signerKeyPairFilename).isFile()) {
+			gsk.keyGen(keyGenParameters);
+			persistenceUtil.write(gsk, signerKeyPairFilename);
+			persistenceUtil.write(gsk.getPublicKey(), signerPKFilename);
+		} else {
+			gsk = (SignerKeyPair) persistenceUtil.read(signerKeyPairFilename);
 		}
 
-		public void setupIssuing(String sigmaFilename) throws IOException, ClassNotFoundException, EncodingException, ProofStoreException, NoSuchAlgorithmException, ImportException, VerificationException {
-
-			String signerKeyPairFilename = "SignerKeyPair-" + l_n + ".ser";
-			String signerPKFilename = "SignerPublicKey-" + l_n + ".ser";
-			String ekpFilename = "ExtendedKeyPair-" + l_n + "-" + l_V + "-" + l_E + ".ser";
-			String epkFilename = "ExtendedPublicKey-" + l_n + "-" + l_V + "-" + l_E + ".ser";
-
-			persistenceUtil = new FilePersistenceUtil();
-			gsk = new SignerKeyPair();
-
-			keyGenParameters = KeyGenParameters.createKeyGenParameters(l_n, 1632, 256, 256, 1, 597, 120, 2724, 80, 256, 80, 80);
-
-			graphEncParams = new GraphEncodingParameters(l_V, 56, l_E, 256, 16);
-
-			if (!new File(signerKeyPairFilename).isFile()) {
-				gsk.keyGen(keyGenParameters);
-				persistenceUtil.write(gsk, signerKeyPairFilename);
-				persistenceUtil.write(gsk.getPublicKey(), signerPKFilename);
-			} else {
-				gsk = (SignerKeyPair) persistenceUtil.read(signerKeyPairFilename);
-			}
-
-			if (!new File(ekpFilename).isFile()) {
-				ekp = new ExtendedKeyPair(gsk, graphEncParams, keyGenParameters);
-				ekp.setupEncoding();
-				ekp.generateBases();
-				ekp.createExtendedKeyPair();
-				persistenceUtil.write(ekp, ekpFilename);
-			} else {
-				ekp = (ExtendedKeyPair) persistenceUtil.read(ekpFilename);
-			}
-
-			epk = ekp.getExtendedPublicKey();
-			persistenceUtil.write(ekp.getExtendedPublicKey(), epkFilename);
-			baseCollection = epk.getBaseCollection();
-
-			//		System.out.println("basecollection length: " + baseCollection.size());
-			//		System.out.println("l_V: " + ekp.getGraphEncodingParameters().getL_V());
-			//		System.out.println("l_E: " + ekp.getGraphEncodingParameters().getL_E());
-			//		System.out.println("vertex bases: " + epk.getEncoding().);
-
-			String graphFilename = "/Users/alpac/DEV/lib-graph-sig-benchmarks/test.graphml";
-
-			signer = new SignerOrchestrator(ekp, messageGateway);
-			recipient = new RecipientOrchestrator(ekp.getExtendedPublicKey(), messageGateway);
-
-			try {
-				signer.init();
-				recipient.init();
-//				signer.round0();
-//				recipient.round1();
-//				signer.round2();
-//				recipient.round3();
-//				recipient.serializeFinalSignature(sigmaFilename);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		if (!new File(ekpFilename).isFile()) {
+			ekp = new ExtendedKeyPair(gsk, graphEncParams, keyGenParameters);
+			ekp.setupEncoding();
+			ekp.generateBases();
+			ekp.createExtendedKeyPair();
+			persistenceUtil.write(ekp, ekpFilename);
+		} else {
+			ekp = (ExtendedKeyPair) persistenceUtil.read(ekpFilename);
 		}
+
+		epk = ekp.getExtendedPublicKey();
+		persistenceUtil.write(ekp.getExtendedPublicKey(), epkFilename);
+		BaseCollection baseCollection = epk.getBaseCollection();
+
+		//		System.out.println("basecollection length: " + baseCollection.size());
+		//		System.out.println("l_V: " + ekp.getGraphEncodingParameters().getL_V());
+		//		System.out.println("l_E: " + ekp.getGraphEncodingParameters().getL_E());
+		//		System.out.println("vertex bases: " + epk.getEncoding().);
+
+		String graphFilename = "signer-infra-50.graphml";
+
+		signer = new SignerOrchestrator(graphFilename, ekp, messageGateway);
+		recipient = new RecipientOrchestrator(graphFilename, ekp.getExtendedPublicKey(), messageGateway);
+
+		try {
+			signer.init();
+			recipient.init();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@State(Scope.Benchmark)
 	@OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -170,14 +150,12 @@ public class IssuingBenchmark  {
 		@Setup(Level.Invocation)
 		public void setup() throws ClassNotFoundException, ExecutionException, EncodingException, InterruptedException, IOException, ProofStoreException, NoSuchAlgorithmException, VerificationException, ImportException, NoSuchFieldException, IllegalAccessException {
 			super.setup();
-
 		}
 
 		@Benchmark
 		@BenchmarkMode({Mode.AverageTime})
 		@OutputTimeUnit(TimeUnit.MILLISECONDS)
 		public void round0(Round0Benchmark state) throws Exception {
-
 			signer.round0();
 		}
 
@@ -197,7 +175,6 @@ public class IssuingBenchmark  {
 
 		@Setup(Level.Invocation)
 		public void setup() throws ClassNotFoundException, ExecutionException, EncodingException, InterruptedException, IOException, ProofStoreException, NoSuchAlgorithmException, VerificationException, ImportException, NoSuchFieldException, IllegalAccessException {
-			String sigmaFilename = "signer-infra-" + ".ser";
 			super.setup();
 			signer.round0();
 		}
@@ -243,7 +220,6 @@ public class IssuingBenchmark  {
 			recipient = null;
 		}
 	}
-
 
 	@State(Scope.Benchmark)
 	@OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -302,7 +278,7 @@ public class IssuingBenchmark  {
 			recipient = null;
 		}
 	}
-	
+
 	@State(Scope.Benchmark)
 	@OutputTimeUnit(TimeUnit.MILLISECONDS)
 	public static class SerializeGSBenchmark extends IssuingBenchmark {
@@ -336,12 +312,12 @@ public class IssuingBenchmark  {
 	public static void main(String[] args) throws FileNotFoundException, RunnerException {
 		Options opt = new OptionsBuilder()
 				.include(eu.prismacloud.primitives.grs.bench.IssuingBenchmark.class.getSimpleName())
-				.param("l_n", "512", "1024")//, "2048", "3072")
-				.param("bases", "100")//, "1000", "10000", "100000")
+				.param("l_n", "512")//, "1024")//, "2048", "3072")
+				.param("bases", "200")//, "2000", "20000", "200000")
 				.jvmArgs("-server")
 				.warmupIterations(0)
 //				.addProfiler(SolarisStudioProfiler.class)
-				.warmupForks(1)
+				.warmupForks(0)
 				.measurementIterations(1)
 				.threads(1)
 				.forks(1)
