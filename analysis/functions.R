f977b1825c6c03444953bba6168fdc9f80396aab
Ioannis Sfyrakis <ioannis.sfyrakis@newcastle.ac.uk>
2019-02-10 02:24:15 +0000

    update analysis of issuing
    added functions for creating boxplots and barplots

--- analysis/functions.R
+++ analysis/functions.R
@@ -18,3 +18,38 @@ getFilePath <- function(folderPath, fileName, extension){
   filePath <- paste0(folderPath, fileName, extension, collapse = "" )
   return(filePath)
 }
+
+## creates ordered boxplots with jitter
+jitterOrderedBoxplot <- function(dataFrame, dx , dy, xlabel, ylabel, flabel){
+  p <- ggplot(dataFrame, aes(x = reorder(factor(dx), dy), y = dy)) +
+    stat_boxplot(geom = 'errorbar', color = "black") +
+    geom_boxplot(fill = factor(dataFrame$KeyLength), color = "black", notch = FALSE) +
+    geom_point(position = "jitter", color = "blue", alpha = .5) +
+    geom_rug( color = "black") + 
+    labs( x = xlabel,  y = ylabel, fill = flabel) + 
+    coord_flip()
+  return(p)
+}
+
+## creates an ordered barplot that calculates the mean 
+orderedMeanBarplot <- function(dataframe, dx, dy, xlabel, ylabel) {
+  p <- ggplot(dataframe, aes(x = reorder(factor(dx), dy), y = dy)) +
+    geom_bar(stat = "summary", fun.y = "mean", position = "dodge", fill = 'blue')  +
+    labs( x=xlabel,  y = ylabel) +  coord_flip() +  theme(panel.grid.major = element_blank()) 
+  return(p)
+}
+
+## read csv files
+readCSVs <- function(a_csv) {
+  print(a_csv)
+  the_data <- read.csv(a_csv, header = TRUE, sep = ",", stringsAsFactors=FALSE)
+}
+
+## create a list of dataframes from csv files
+getCSVData <- function(dPattern, path, fPattern ){
+  dirs <- grep(dPattern,list.dirs(path,recursive=FALSE),value=TRUE)
+  lfiles <- list.files(dirs, pattern = fPattern, recursive = TRUE, full.names = TRUE, include.dirs = TRUE)
+  data <- lapply(lfiles, FUN = readCSVs)
+  return(data)
+}
+
--- analysis/issuing.R
+++ analysis/issuing.R
@@ -4,15 +4,7 @@ source("packages.R")
 source("functions.R")
 source("configuration.R")
 
-# # read data
-# dataFile <- "results-commitment-raw-2019-01-13_01-20-05"
-# dataPath <- getFilePath("../data/", dataFile, ".csv")
-# commitmentData <- read.csv(dataPath, sep=",")
-
-(dirs <- grep("issuing-400-512*",list.dirs("../data/issuing-profile-50-csv",recursive=FALSE),value=TRUE))
-(lfiles <- list.files(dirs, pattern = "Method-list--CPU\\.csv", recursive = TRUE, full.names = TRUE, include.dirs = TRUE))
-
-list.dirs("../data",pattern= "issuing-400-512", recursive = TRUE )
+#list.dirs("../data",pattern= "^issuing-400-512", recursive = TRUE )
 
 folderPattern512 = "^issuing-400-512"
 folderPattern1024 = "^issuing-400-1024"
@@ -20,31 +12,60 @@ folderPattern2048 = "^issuing-400-2048"
 folderPattern3072 = "^issuing-400-3072"
 
 folders <- dir("../data/issuing-profile-50-csv", pattern = folderPattern512 , full.names = TRUE, ignore.case = TRUE)
-lapply(folders, 1, function(folders){
-  folders
-})
 
-readCSVs <- function(a_csv) {
-  print(a_csv)
-   the_data <- read.csv(a_csv, header = TRUE, sep = ",", stringsAsFactors=FALSE)
-}
+
+(dirs <- grep("issuing-400-512*",list.dirs("../data/issuing-profile-50-csv",recursive=FALSE),value=TRUE))
+
+(lfiles <- list.files(dirs, pattern = "Method-list--CPU\\.csv", recursive = TRUE, full.names = TRUE, include.dirs = TRUE))
+
+(dirs1024 <- grep("issuing-400-1024*",list.dirs("../data/issuing-profile-50-csv",recursive=FALSE),value=TRUE))
+
+(lfiles1024 <- list.files(dirs1024, pattern = "Method-list--CPU\\.csv", recursive = TRUE, full.names = TRUE, include.dirs = TRUE))
+
+
 
 str(d)
 (data <- lapply( lfiles, FUN = readCSVs))
 
-(dfd <- data[1])
 
-df <- ldply (dfd, data.frame)
-str(df$Method)  
-(df$Method <- as.character(df$Method))
 
-(filtered <- df[with(df, grepl("eu.prismacloud.primitives.zkpgs.*",df$Method) | grepl("java.math.BigInteger.*",df$Method)), ])
+(data512 <- getCSVData("issuing-400-512*","../data/issuing-profile-50-csv", "Method-list--CPU\\.csv"))
+# create a dataframe from the experiments
+issuing512 <- bind_rows(data512, .id="expID")
+issuing512['KeyLength'] = 512
+
+(data1024 <- getCSVData("issuing-400-1024*","../data/issuing-profile-50-csv", "Method-list--CPU\\.csv"))
+issuing1024 <- bind_rows(data1024, .id="expID")
+issuing1024['KeyLength'] = 1024
+
+(data2048 <- getCSVData("issuing-400-2048*","../data/issuing-profile-50-csv", "Method-list--CPU\\.csv"))
+issuing2048 <- bind_rows(data2048, .id="expID")
+issuing2048['KeyLength'] = 2048
+
+(data3072 <- getCSVData("issuing-400-3072*","../data/issuing-profile-50-csv", "Method-list--CPU\\.csv"))
+issuing3072 <- bind_rows(data3072, .id="expID")
+issuing3072['KeyLength'] = 3072
+
+datafull <- rbind(issuing512, issuing1024)
+datafull <- rbind(datafull, issuing2048)
+datafull <- rbind(datafull, issuing3072)
+
+
+
+# (filtered <- issuing512[with(issuing512, grepl("eu.prismacloud.primitives.zkpgs.*",issuing512$Method) | grepl("java.math.BigInteger.*",issuing512$Method)), ])
+
+(filtered <- datafull[with(datafull, grepl("eu.prismacloud.primitives.zkpgs.*",datafull$Method)), ])
+
 
 # rename headings 
-col_headings <- c('Method','Time_ms ', 'OwnTime_ms')
+col_headings <- c('ExpID','Method','Time_ms ', 'OwnTime_ms', 'KeyLength')
 names(filtered) <- col_headings
 str(filtered)
 
+# truncate method names
+(filtered$Method <- gsub(pattern = "eu.prismacloud.primitives.zkpgs.", replacement = "", filtered$Method)) 
+(filtered$Method <- gsub(pattern = " (/.)*[a-zA-Z]*.java", replacement = "", filtered$Method)) 
+(filtered$Method <- gsub(pattern = "\\(.*?\\)", replacement = "", filtered$Method)) 
 
 scatter.smooth(x=factor(filtered$Method), y=filtered$Time_ms, main="Method ~ Time_ms")  # scatterplot
 
@@ -66,53 +87,16 @@ str(filtered$`Time_ms `)
 length(filtered$`Time_ms `)
 length(filtered$Method)
 
-(filtered$Method <- gsub(pattern = "eu.prismacloud.primitives.zkpgs.", replacement = "", filtered$Method)) 
-
-(filtered$Method <- gsub(pattern = " (/.)*[a-zA-Z]*.java", replacement = "", filtered$Method)) 
-(filtered$Method <- gsub(pattern = "\\(.*?\\)", replacement = "", filtered$Method)) 
-length(filtered$Method)
 head(filtered)
 
-ggplot(filtered, aes(x=filtered$Method, y=filtered$Time_ms)) +
-  stat_boxplot(geom ='errorbar', color="black") +
-  geom_boxplot(fill="cornflowerblue", color="black", notch=FALSE) +
-  geom_point(position="jitter", color="blue", alpha=.5) +
-  geom_rug( color="black") + 
-  labs( x="Method",  y = "cpu  time (msec)", fill="Graph size") + 
-  coord_flip() #+ theme(axis.text.y = element_text( hjust = 0.95, vjust = 0.1)) # + scale_x_discrete( labels= abbreviate, expand=c(0, 0)) 
+# filter dataframe to include only methods with cpu time > 50 ms
+filtered80 <- filter(filtered, filtered$Time_ms > 50)
 
-ggplot(filtered, aes(x=reorder(filtered$Method, filtered$Time_ms), 
-                     y=filtered$Time_ms)) +
-       #stringr::str_wrap(filtered$Method, 15), filtered$Timems) + xlab(NULL) + 
-  # theme(axis.text.x = element_text( size = 9,
-  #       color = "black", face = "plain", vjust = 1, hjust = 1),
-  #       plot.margin = margin(10, 10, 10, 100))
-  # ylab("Method") + scale_x_discrete(labels = abbreviate) +  
-  #stat_boxplot(geom ='errorbar', color="black") +
-  geom_bar(stat = "identity", position = "dodge", fill = 'blue')  +
-  labs( x="Method",  y = "CPU time (msec)") +  coord_flip()
+(jitterOrderedBoxplot(filtered80, filtered80$Method, filtered80$Time_ms, "Method names", "CPU time (ms)", "") )
 
-# calculate means
-#(means<- round(tapply(filtered$Score, commitmentData$KeyLength, mean), digits=2)) 
+(orderedMeanBarplot(filtered80, filtered80$Method, filtered80$Time_ms, "Method names", "CPU time (ms)"))
 
 # plot means
 plotmeans(filtered$Time_ms ~ filtered$Method, digits=3, ccol="red", xlab = "Methods", ylab="CPU time for issuing methods (msec)",las=2, cex.axis=0.4, mean.labels=F, mar=c(10.1,4.1,4.1,2.1),n.label = FALSE, mgp=c(3,1,0) , main="Plot of issuing methods")
 
-str(filtered)
-(filtered$`Time_ms `)
-mm <- as.character(filtered$Method)
-str(mm)
-tt <- filtered$Time_ms
-ggbarplot(filtered, x=mm, y=tt,
-          fill = "blue"               # change fill color by cyl
-          # color = "white",            # Set bar border colors to white
-          # palette = "jco",            # jco journal color palett. see ?ggpar
-          # sort.val = "asc",           # Sort the value in dscending order
-          # sort.by.groups = FALSE,      # Sort inside each group
-          # x.text.angle = 90           # Rotate vertically x axis texts
-          )
-
-ggbarplot(filtered, x= filtered$Method, y=filtered$Time_ms, orientation = "horiz")
-
-(stable.p <- ggtexttable(filtered, rows = NULL, theme = ttheme("mBlueWhite")))
-
+(stable.p <- ggtexttable(filtered80, rows = NULL, theme = ttheme("mBlueWhite")))
