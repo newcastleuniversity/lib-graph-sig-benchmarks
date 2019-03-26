8ca1f8fa439cc5996c95cf9ee0cf0faf32deedc9
Ioannis Sfyrakis <ioannis.sfyrakis@newcastle.ac.uk>
2019-03-26 20:54:37 +0000

    add plot for displaying cpu time for proving methods
    add plot for displaying cpu time for verifying methods

--- analysis/proving.R
+++ analysis/proving.R
@@ -1,4 +1,6 @@
 # proving computation results analysis
+setwd("~/DEV/lib-graph-sig-benchmarks/analysis")
+
 ## load required packages, auxiliary functions and configuration
 source("packages.R")
 source("functions.R")
@@ -26,7 +28,7 @@ ggplot(filtered_50, aes(x = filtered_50$Time_ms)) +
 
 (uniqueM <- unique(filtered_50$Method))
 
-dProving <- filterProving(filtered_50)
+dProving <- filtered_50 #filterProving(filtered_50)
 
 (jitterOrderedBoxplot(dProving, dProving$Method, dProving$Time_ms, dProving$KeyLength, "Methods", "CPU time (ms)", "") )
 savePlot(last_plot(), "proving-jitter-ordered-boxplot.pdf")
@@ -50,7 +52,7 @@ proving_500 <- renameHeadings(proving_500, c('ExpID','ExpID.1', 'Method','Time_m
 str(proving_500)
 
 filtered_500 <- filterMethods(proving_500)
-dProving_500 <- filterProving(filtered_500)
+dProving_500 <- filtered_500 #filterProving(filtered_500)
 
 (jitterOrderedBoxplot(dProving_500, dProving_500$Method, dProving_500$Time_ms, dProving_500$KeyLength, "Methods", "CPU time (ms)", "") )
 savePlot(last_plot(), "proving-jitter-ordered-boxplot-500.pdf")
@@ -85,3 +87,49 @@ ol <- createOrderedLinePlots(dProving_50_500, dProving_50_500$Method, dProving_5
     scale_linetype_manual(values=c("solid","twodash", "dotted", "dashed")) +
     theme(axis.text.x = element_text(angle = 77, vjust = 1, hjust=1)))
 savePlot(last_plot(), "proving-facet-lineplot-50-500.pdf")
+
+# calculate total proving time from ProverOrchestrator
+## init() + executePreChallengePhase + computeChallenge + executePostChallenge
+
+pmethods <- c("ProverOrchestrator.init", "ProverOrchestrator.executePreChallengePhase", "ProverOrchestrator.executePostChallengePhase", "ProverOrchestrator.computeChallenge", "ProverOrchestrator.computeCommitmentProvers", "ProverOrchestrator.computePairWiseProvers", "ProverOrchestrator.computeTildeZ")
+
+# grepl(paste(pmethods, collapse = "|"), my_text)
+
+(pOrchestrator <- dProving_50_500[with(dProving_50_500, grepl(paste(pmethods, collapse = "|"), dProving_50_500$Method)), ])
+
+
+# detach plyr so we can work with dplyr otherwise the summation per group doesn't work
+detach(package:plyr)    
+library(dplyr)
+(pOrchestrator_filtered <- pOrchestrator %>% 
+    group_by(KeyLength, Vertices) %>%
+    summarise(ProvingTime = sum(mean)))
+
+ggplot(pOrchestrator_filtered, aes(x = reorder(factor( pOrchestrator_filtered$Vertices), pOrchestrator_filtered$ProvingTime), y = pOrchestrator_filtered$ProvingTime, group= pOrchestrator_filtered$KeyLength)) +
+  geom_bar(stat = "summary", fun.y = "sum", position = "dodge", aes(fill = factor( pOrchestrator_filtered$KeyLength)))  +
+  facet_grid(factor( pOrchestrator_filtered$KeyLength), margins = FALSE,  scales = "free", space = "free") +
+  labs( x = "Graph size (number of vertices)",  y = "Proving time (ms)", fill = "") +
+  # coord_flip() +  
+  theme_bw()
+
+ggplot() +
+  geom_line(aes(x=factor(pOrchestrator_filtered$Vertices), y=pOrchestrator_filtered$ProvingTime, group=pOrchestrator_filtered$KeyLength, color=factor(pOrchestrator_filtered$KeyLength)), pOrchestrator_filtered) +
+  # geom_point(data=pOrchestrator_filtered, aes(x=factor(pOrchestrator_filtered$Vertices), y=pOrchestrator_filtered$ProvingTime, color=factor(pOrchestrator_filtered$KeyLength),    shape=factor(pOrchestrator_filtered$KeyLength))) + 
+  geom_line(aes(x=factor(pOrchestrator$Vertices), y=pOrchestrator$mean, group=factor(pOrchestrator$Method), color=factor(pOrchestrator$Method)), pOrchestrator) + 
+  
+  # geom_line(aes(x=factor(signer_issuing_time$Vertices), y=signer_issuing_time$IssuingTime, group=signer_issuing_time$KeyLength, color=factor(signer_issuing_time$KeyLength), linetype=factor(signer_issuing_time$KeyLength)), signer_issuing_time) +
+  # geom_point(data=signer_issuing_time, aes(x=factor(signer_issuing_time$Vertices), y=signer_issuing_time$IssuingTime), color=factor(signer_issuing_time$KeyLength), shape=factor(total_issuing_time$KeyLength), size=2, fill="black") +
+  
+  # facet_grid(pOrchestrator_filtered$KeyLength) +
+  labs( x = "Graph size (number of vertices)",  y = "Proving time (ms)", color = "", shape = "Key length") +
+  theme_bw()
+
+ggplot(pOrchestrator, aes(x = reorder(factor( pOrchestrator$Vertices), pOrchestrator$mean), y = pOrchestrator$mean, group= pOrchestrator$Method)) +
+  geom_bar(stat = "summary", fun.y = "mean", position = "dodge", aes(fill = factor( pOrchestrator$Method)))  +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.25, position=position_dodge(0.9)) +
+  facet_grid(factor( pOrchestrator$KeyLength), margins = FALSE,  scales = "free", space = "free") +
+  labs( x = "Graph size (number of vertices)",  y = "Proving time (ms)", fill = "") +
+  # coord_flip() +  
+  theme_bw()
+
+
