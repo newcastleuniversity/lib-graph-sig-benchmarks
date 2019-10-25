d7f11cc71967625badf63c47064aceec85af934b
Ioannis Sfyrakis <ioannis.sfyrakis@newcastle.ac.uk>
2019-10-25 12:40:31 +0100

    update configuration for R

--- analysis/configuration.R
+++ analysis/configuration.R
@@ -1,1 +1,8 @@
-options(digits=10)
+options(digits = 9)
+options("scipen" = 10)
+paperDataFolderPath <- "/Users/alpac/Library/Mobile Documents/com~apple~CloudDocs/lib-graph-sig-journal/data/"
+paperFigureFolderPath <- "/Users/alpac/Library/Mobile Documents/com~apple~CloudDocs/lib-graph-sig-journal/figures/"
+paperAnalysisFolderPath <- "/Users/alpac/Library/Mobile Documents/com~apple~CloudDocs/lib-graph-sig-journal/analysis/"
+
+# File <- "results-keygen-raw-2018-11-13_11-52-31"
+# paperPath <- paste0("../data/", dataFile, ".csv", collapse = "" )
